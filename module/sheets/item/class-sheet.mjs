d2dc2fe4b309ceaa0f8325de99196e8074b34910
Jonathan Landicheff <jlandicheff@kilig.io>
2025-11-07 19:19:21 +0100

    feat: list callings in ClassSheet

--- module/sheets/item/class-sheet.mjs
+++ module/sheets/item/class-sheet.mjs
@@ -1,3 +1,4 @@
+import { enrichHTML } from "../../utils/text-editor.mjs";
 import BaseItemSheet from "./base-item-sheet.mjs";
 
 export default class ClassSheet extends BaseItemSheet {
@@ -21,8 +22,42 @@ export default class ClassSheet extends BaseItemSheet {
             perk: await this._prepareReference("system.perk", "perk"),
             perks: await this._prepareReferenceList("system.perks", "perk"),
             factions: await this._prepareReferenceList("system.factions", "faction"),
+            callings: await this._prepareCallings(),
         });
 
         return context;
+    }
+
+    async _prepareCallings() {
+        const callings = await globalThis.registry.getAllOfType("calling");
+        let openCallings = [];
+        let classCallings = [];
+
+        callings.forEach((calling) => {
+            if (calling.system.isOpen) {
+                openCallings.push(calling);
+            } else {
+                const inClass = calling.system.preconditions.some((conditions) => {
+                    return conditions.some((condition) => condition.type === "class" && condition.slug === this.item.system.slug);
+                });
+
+                if (inClass) {
+                    classCallings.push(calling);
+                }
+            }
+        });
+
+        const mapFn = async (item) => ({
+            html: await enrichHTML(`@SLUG[calling:${item.system.slug}]`),
+            name: item.name
+        });
+
+        classCallings = await Promise.all(classCallings.map(mapFn));
+        openCallings = await Promise.all(openCallings.map(mapFn));
+
+        return {
+            classCallings: classCallings.sort((a, b) => a.name.localeCompare(b.name)).map(calling => calling.html),
+            openCallings: openCallings.sort((a, b) => a.name.localeCompare(b.name)).map(calling => calling.html),
+        };
     }
 }
